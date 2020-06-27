b206da537dba055a4c0d72a1307032495f434992
Adriano Valadares <45238681+Valadares-A@users.noreply.github.com>
2020-06-26 23:19:00 -0300

    notes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  text: string = "Start with Ionic";
+  name: string = "";
+  number: number;
+  list = [
+    {
+      name: "Adriano",
+      number: 77272,
+    },
+  ];
 
   constructor() {}
 
+  onClickBoy() {
+    this.text = "Ha ha! Bruh!!!";
+  }
+
+  onNamechange(value) {
+    this.name = value;
+  }
+  onNumberChange(value) {
+    this.number = value;
+  }
+  onAdd() {
+    this.list.push({ name: this.name, number: this.number });
+    console.log(this.list);
+    this.onClear();
+  }
+  onClear() {
+    this.name = "";
+    this.number = null;
+  }
 }
